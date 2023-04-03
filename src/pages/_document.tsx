fadb6ff7f9098f0054ae34596f9abdabd31acdf8
Aaron-liushuai <Aaron.liu@aelf.io>
2023-04-03 19:44:52 +0800

    feat: Updated some configuration and logic funciton

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,30 @@
+export type QRCodeDataObjType = {
+  address: string;
+  netWorkType: string;
+  chainType: string;
+  type: 'login' | 'send';
+  toInfo: { name: string; address: string };
+  deviceType?: number;
+  assetInfo: {
+    symbol: string;
+    tokenContractAddress: string;
+    chainId: string;
+    decimals: string | number;
+  };
+};
+
+export type QrCodeDataArrType = [
+  string, // chainType
+  string, // netWorkType
+  'login' | 'send', // type
+  string, // toAddress
+  string, // symbol
+  string, // tokenContractAddress
+  string, // chainId
+  string | number, // decimals
+  number | undefined, //
+];
+
 export const copy = (content: string) => {
   const input = document.createElement('input');
   input.value = content;
@@ -10,4 +37,19 @@ export const copy = (content: string) => {
 // check if the user is on mobile
 export const isMobile = (uaString: string) => {
   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(uaString);
+};
+
+export const shrinkSendQrData = (data: QRCodeDataObjType): QrCodeDataArrType => {
+  // 1.chainType  2.netWorkType 3.data.type 4.toAddress 5. symbol 6. tokenContractAddress 7. chainId 8. decimals
+  return [
+    data.chainType,
+    data.netWorkType,
+    data.type,
+    data.address,
+    data.assetInfo.symbol,
+    data.assetInfo.tokenContractAddress,
+    data.assetInfo.chainId,
+    data.assetInfo.decimals,
+    data.deviceType,
+  ];
 };
