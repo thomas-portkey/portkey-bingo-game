dd36c59bb2bd47083c95ea6ae17d7d80b647ec81
Aaron-liushuai <Aaron.liu@aelf.io>
2023-04-04 20:11:30 +0800

    feat: v1.2.x

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -1,4 +1,4 @@
-// export const bingoAddress = '2sGZFRtqQ57F55Z2KvhmoozKrf7ik2htNVQawEAo3Vyvcx9Qwr';
-export const bingoAddress = 'JvDB3rguLJtpFsovre8udJeXJLhsV1EPScGz2u1FFneahjBQm';
-// export const CHAIN_ID = 'tDVW';
-export const CHAIN_ID = 'tDVV';
+export const bingoAddress = 'CuHsvBBBEtV3vManBWUsCmwWs7ANag3Mk8UUbPN7s1waFVa8G';
+// export const bingoAddress = 'JvDB3rguLJtpFsovre8udJeXJLhsV1EPScGz2u1FFneahjBQm';
+export const CHAIN_ID = 'tDVW';
+// export const CHAIN_ID = 'tDVV';
