10c2eaea0b78bc0fbc9ca15e289ec74b17700da1
Ian-potter <120541625+Ian-potter@users.noreply.github.com>
2023-04-08 17:12:47 +0800

    Update rewrites.js
    change proxy api

--- rewrites.js
+++ rewrites.js
@@ -10,7 +10,7 @@
 module.exports = [
   { source: '/api/:path*', destination: 'https://did-portkey-test.portkey.finance/api/:path*' },
   {
-    source: '/AElfIndexer_DApp/:path*',
-    destination: 'https://dapp-portkey-test.portkey.finance/AElfIndexer_DApp/:path*',
+    source: '/Portkey_DID/PortKeyIndexerCASchema/:path*',
+    destination: 'https://dapp-portkey-test.portkey.finance/Portkey_DID/PortKeyIndexerCASchema/:path*',
   },
 ];
